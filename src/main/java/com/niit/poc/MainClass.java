74b21b1956b49cd89fb28c8e86814fce8fb0dae1
Dell Pc <Dell Pc@Dell>
2018-07-29 17:32:47 +0530

    1st commit

---
+++ src/main/java/com/niit/poc/MainClass.java
@@ -0,0 +1,17 @@
+package com.niit.poc;
+
+public class MainClass {
+
+	public int add(int x, int y) {
+		return x+y;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	MainClass mc = new MainClass();
+	
+	mc.add(10, 20);
+
+	}
+
+}
